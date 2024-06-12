0a5b47134c8ff8bfb7c576c8d1af1863dee33ef1
ffidolen <idolenkov@gmail.com>
2024-06-12 20:38:42 +0300

    driver_opt

--- conftest.py
+++ conftest.py
@@ -1,14 +1,20 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import allure
 
 
 @pytest.fixture(scope="session")
-def t_browser(request):
+def chr_browser(request):
     with allure.step("Получен экземпляр браузера"):
-        service= Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
+        options = Options()
+        # options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size+1920,1080")
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
         request.addfinalizer(driver.quit)
         return driver
